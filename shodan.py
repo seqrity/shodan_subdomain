7ced41de3dec001f8b48d0e51ac63c6d933312d6
Seqrity <52572301+seqrity@users.noreply.github.com>
2022-04-25 10:12:35 -0500

    Add files via upload

---
+++ shodan.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import sys
+
+domain = sys.argv[1]
+
+URL= 'https://www.shodan.io/domain/'+domain
+
+response_page = requests.get(URL)
+my_soup = BeautifulSoup(response_page.content, 'html.parser')
+subs = my_soup.find_all('div','card card-padding card-light-blue')
+
+for sub in subs:
+    # print(sub)
+    find = re.findall('<li>.\w+',str(sub)) 
+    match = re.sub('<li>|\[|\]|\'','',str(find))
+    final = re.sub('\, ','\n',str(match))
+    print(final)
